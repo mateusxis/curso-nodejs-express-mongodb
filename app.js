e05f791eab4c90d7255da692eac49e3b304e0eed
mateusxis <mateusx@poli.ufrj.br>
2020-04-01 20:27:56 -0300

    feat: :sparkles: add first routes

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.get('/', (req, res) => {
+  return res.send({ message: 'O método GET está funcionando corretamente.' });
+});
+
+app.post('/', (req, res) => {
+  return res.send({ message: 'O método POST está funcionando corretamente.' });
+});
+
 app.listen(3000);
 
 module.exports = app;
