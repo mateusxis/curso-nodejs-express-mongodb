499fbabe0e817db902115ef3c9d900931626ca20
mateusxis <mateusx@poli.ufrj.br>
2020-04-01 20:49:46 -0300

    feat: :sparkles: add req.query

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.send({ message: 'O método GET está funcionando corretamente.' });
+  const obj = req.query;
+  return res.send({ message: `O método GET está funcionando corretamente. Eu sou ${obj.nome}, tenho ${obj.idade} anos!` });
 });
 
 app.post('/', (req, res) => {
