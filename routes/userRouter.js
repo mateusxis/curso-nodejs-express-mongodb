9e741fe80a81f245b1daef2b8b0c757574b0271b
mateusxis <mateusx@poli.ufrj.br>
2020-04-10 12:00:21 -0300

    feat: :sparkles: add methods of model user

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,27 +1,72 @@
 const express = require('express');
+const UserModel = require('../model/user');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  return res.send({message: 'O método GET do usuário está funcionando corretamente.'});
+  UserModel.find({}, (err, data) => {
+    if (err) return res.send({ error: 'Erro ao buscar os usuários!' });
+
+    return res.send(data);
+  });
 });
 
 router.post('/', (req, res) => {
-  return res.send({message: 'O método POST do usuário está funcionando corretamente.'});
+  const { email, name, password } = req.body;
+
+  if (!email || !name || !password ) return res.send({ error: 'São necessário os campos de name, email e password!' });
+
+  UserModel.findOne({email}, (err, data) => {
+    if (err) return res.send({ error: 'Error ao buscar usuário!' });
+    if (data) return res.send({ error: 'Usuário já existe!' });
+
+    UserModel.create({ email, name, password }, (err, data) => {
+      if (err) return res.send({ error: 'Error ao salvar usuário!' });
+
+      data.password = undefined;
+
+      return res.send(data);
+    });
+  });
 });
 
 router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  return res.send({message: `O método GET do usuário ${id} está funcionando corretamente.`});
+  const { id } = req.params;
+
+  if (!id) return res.send({ error: 'É necessário o id do usuário!' });
+
+  UserModel.findById(id, (err, data) => {
+    if (err) return res.send({ error: 'Error ao buscar usuário!' });
+
+    return res.send(data);
+  })
 });
 
 router.put('/:id', (req, res) => {
-  const id = req.params.id;
-  return res.send({message: `O método PUT do usuário ${id} está funcionando corretamente.`});
+  const { id } = req.params;
+  const { email, name, password } = req.body;
+
+  if (!id) return res.send({ error: 'É necessário o id do usuário!' });
+  if (!email && !name && !password ) return res.send({ error: 'É necessário os campos de name ou email ou password!' });
+  
+  UserModel.findByIdAndUpdate(id, {email, name, password}, (err, data) => {
+    if (err) return res.send({ error: 'Error ao atualizar o usuário!' });
+
+    return res.send(data);
+  })
+
 });
 
 router.delete('/:id', (req, res) => {
-  const id = req.params.id;
-  return res.send({message: `O método DELETE do usuário ${id} está funcionando corretamente.`});
+  const { id } = req.params;
+
+  if (!id) return res.send({ error: 'É necessário o id do usuário!' });
+
+  UserModel.findByIdAndDelete(id, (err, data) => {
+    if (err) return res.send({ error: 'Error ao deletar o usuário!' });
+
+    return res.send(data);
+  })
 });
 
 module.exports = router;
