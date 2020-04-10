a73d597a001c8a99d92c573bdabad1111635e1c2
mateusxis <mateusx@poli.ufrj.br>
2020-04-10 13:40:33 -0300

    feat: :sparkles: create router auth

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
+const authRouter = require('./authRouter');
 const userRouter = require('./userRouter');
 
 module.exports = app => {
+  app.use('/auth', authRouter)
   app.use('/users', userRouter)
 }
 
