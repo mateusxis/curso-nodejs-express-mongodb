f75654ae7d944844005d854c499cb9ca0df7e6ca
mateusxis <mateusx@poli.ufrj.br>
2020-04-15 14:53:50 -0300

    fix: :bug: add try catch

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,12 +6,14 @@ const auth = (req, res, next) => {
   if (!token_header) return res.status(400).send({ error: 'Token não enviado!' });
   if (typeof token_header !== 'string') return res.status(400).send({ error: 'Token não está com tipo de variável correto!' });
 
-  jwt.verify(token_header, 'ingresso.com2020', (err, decoded) => {
-    if (err) res.status(401).send({ error: 'Token inválido!' });
+  try {
+    const decoded = jwt.verify(token_header, jwt_pass);
 
     res.locals.auth_data = decoded;
     return next();
-  })
+  } catch (err) {
+    return res.status(401).send({ error: 'Token inválido!' });
+  }
 }
 
 module.exports = auth;
