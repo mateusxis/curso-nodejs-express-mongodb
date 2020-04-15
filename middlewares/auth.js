0a1026d7f0e1ed4a9b5af3085e0cbdd632754a58
mateusxis <mateusx@poli.ufrj.br>
2020-04-15 14:18:02 -0300

    fix: :bug: typeof auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ const auth = (req, res, next) => {
   const token_header = req.headers.auth;
 
   if (!token_header) return res.send({ error: 'Token não enviado!' });
-  if (typeof token_header === 'string') return res.send({ error: 'Token não está com tipo de variável correto!' });
+  if (typeof token_header !== 'string') return res.send({ error: 'Token não está com tipo de variável correto!' });
 
   jwt.verify(token_header, 'ingresso.com2020', (err, decoded) => {
     if (err) res.send({ error: 'Token inválido!' });
